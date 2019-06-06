a11cb70a62a378b9d1a038565787108cd8b6683d
Alex Cho <alexsjcho@gmail.com>
2019-06-05 20:30:21 -0700

    finished pryamid recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  console.log(level);
+  pyramid(n, row, level + add);
+}
+
+console.log(pyramid(3));
+
+module.exports = pyramid;
+
+/* 
+
+iterative solution
+
 function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
   for (let row = 0; row < n; row++) {
@@ -29,6 +56,4 @@ function pyramid(n) {
   }
 }
 
-console.log(pyramid(3));
-
-module.exports = pyramid;
+*/
