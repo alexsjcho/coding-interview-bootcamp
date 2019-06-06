3f2c0aa6a366e3779dbaa788911f813e98fb9b50
Alex Cho <alexsjcho@gmail.com>
2019-06-05 20:43:13 -0700

    finished vowels iterative solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  const isVowels = ['a', 'e', 'i', 'o', 'u'];
+  for (let char of str.toLowerCase()) {
+    if (isVowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(vowels('Yo whats up my man, baby!'));
 
 module.exports = vowels;
