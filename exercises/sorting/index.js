73567eaa41843e7c59ca7fd27dfb51f6bba101c3
Alex Cho <alexsjcho@gmail.com>
2019-07-03 06:20:20 -0700

    finished bubblesort challenge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,24 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+console.log(bubbleSort([50, 33, -1, 0, 100, 20]));
 
-}
+function selectionSort(arr) {}
 
-function mergeSort(arr) {
+function mergeSort(arr) {}
 
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
