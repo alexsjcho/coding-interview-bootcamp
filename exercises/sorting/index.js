c67b767b32f815008fa265c02e9f2c77d0860400
Alex Cho <alexsjcho@gmail.com>
2019-07-03 06:30:31 -0700

    finished selectionsorting challenge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,8 +16,25 @@ function bubbleSort(arr) {
 
 console.log(bubbleSort([50, 33, -1, 0, 100, 20]));
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([50, 33, -1, 0, 100, 20, 1, 2, 13]));
 function mergeSort(arr) {}
 
 function merge(left, right) {}
