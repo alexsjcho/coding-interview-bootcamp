3d2b8c20a562808d46426c27a8eca12bea4b9804
Alex Cho <alexsjcho@gmail.com>
2019-07-03 06:58:35 -0700

    finished mergeSort challenge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -35,8 +35,32 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([50, 33, -1, 0, 100, 20, 1, 2, 13]));
-function mergeSort(arr) {}
 
-function merge(left, right) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+
+console.log(mergeSort([50, 33, -1, 0, 100, 20, 1, 2, 13, -3, -14, 1000, -324]));
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
